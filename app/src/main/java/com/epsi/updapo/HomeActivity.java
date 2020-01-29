0cdfd430b53e6c0ff4fe55ee402b1ed297dd7648
Alexis <alexis.heroin@epsi.fr>
2020-01-29 17:30:27 +0100

    custom home activity

--- app/src/main/java/com/epsi/updapo/HomeActivity.java
+++ app/src/main/java/com/epsi/updapo/HomeActivity.java
@@ -1,12 +1,25 @@
 package com.epsi.updapo;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends UPDAPOActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        findViewById(R.id.buttonZone1).setOnClickListener(this);
+        findViewById(R.id.buttonZone2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonZone1:
+                break;
+            case R.id.buttonZone2:
+                break;
+        }
     }
 }
